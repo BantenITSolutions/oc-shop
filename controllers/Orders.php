0dbf48b955905076bcec667d3ec435d8bb89c8d0
Dave Shoreman <code@dsdev.io>
2014-11-08 09:37:53 +0000

    Add real values
    Updates the order index view to use actual variables for showing
    the scoreboard items, rather than hard-coded numbers. Still got to
    clean a lot of that controller cruft though.

--- controllers/Orders.php
+++ controllers/Orders.php
@@ -2,6 +2,7 @@
 
 use BackendMenu;
 use Backend\Classes\Controller;
+use Carbon\Carbon;
 use Dshoreman\Shop\Models\Order;
 
 /**
@@ -22,6 +23,27 @@ class Orders extends Controller
         parent::__construct();
 
         BackendMenu::setContext('DShoreman.Shop', 'shop', 'orders');
+    }
+
+    public function index()
+    {
+        $this->vars['ordersTotal'] = Order::count();
+        $this->vars['ordersTotalLast'] = 17;
+
+        $this->vars['ordersPaid'] = Order::where('is_paid', 1)->count();
+        $this->vars['ordersPending'] = $this->vars['ordersTotal'] - $this->vars['ordersPaid'];
+
+        $this_month_start = Carbon::now()->startOfMonth();
+        $last_month_start = Carbon::now()->subMonth()->startOfMonth();
+        $last_month_end = Carbon::now()->subMonth()->endOfMonth();
+
+        $this->vars['ordersCount'] = Order::where('created_at', '>=', $this_month_start)->count();
+        $this->vars['ordersCountLast'] = Order::whereBetween('created_at', [$last_month_start, $last_month_end])->count();
+
+        $this->vars['ordersValue'] = Order::where('created_at', '>=', $this_month_start)->sum('total');
+        $this->vars['ordersValueLast'] = Order::whereBetween('created_at', [$last_month_start, $last_month_end])->sum('total');
+
+        return $this->asExtension('ListController')->index();
     }
 
     public function update($recordId, $context = null)
